b55f2668b1719d478c3c9d072a09ab5c8dd977c6
Rehman-codes <rehman.programs@gmail.com>
2024-06-21 16:45:55 +0500

    Completed Close Inspection Functionality

--- content.js
+++ content.js
@@ -2,30 +2,43 @@ let isInspectionActive = false;
 let Box;
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'inspectPage') {
-        inspectPage();
+    if (request.action === 'toggleInspection') {
+        toggleInspection();
     }
+    sendResponse({ isInspectionActive });
 });
 
-function inspectPage() {
+function toggleInspection() {
+    isInspectionActive = !isInspectionActive;
+    if (isInspectionActive) {
+        startInspection();
+    } else {
+        stopInspection();
+    }
+}
 
-    isInspectionActive = true;
-    
+function startInspection() {
     const allElements = document.querySelectorAll('*');
-
     allElements.forEach(node => {
         node.addEventListener('mouseover', beginInspection);
     });
-
     document.addEventListener('mousemove', moveBox);
+    Box.style.display = 'flex';
+    Box.style.flexDirection = 'column';
+}
+
+function stopInspection() {
+    const allElements = document.querySelectorAll('*');
+    allElements.forEach(node => {
+        node.removeEventListener('mouseover', beginInspection);
+    });
+    document.removeEventListener('mousemove', moveBox);
+    Box.style.display = 'none';
 }
 
 function beginInspection(event) {
     updateBox(event);
-    Box.style.display = 'flex';
-    Box.style.flexDirection = "column";
 }
-
 
 function updateBox(event) {
     let nodeName = event.target.nodeName;
@@ -33,21 +46,20 @@ function updateBox(event) {
     let allClasses = Array.from(event.target.classList);
 
     Box.innerHTML = `
-     <div style="border-bottom: 1px solid black; padding: 5px;"><h5 style="color:black;" >Node Name:</h5> ${nodeName}</div>
-     <div style="border-bottom: 1px solid black; padding: 5px;"><h5 style="color:black;" >ID:</h5> ${id || 'N/A'}</div>
-     <div style="border-bottom: 1px solid black; padding: 5px;"><h5 style="color:black;" >Classes:</h5> ${allClasses.join(', ') || 'N/A'}</div>
+     <div style="border-bottom: 1px solid black; padding: 5px;"><h5 style="color:black;">Node Name:</h5> ${nodeName}</div>
+     <div style="border-bottom: 1px solid black; padding: 5px;"><h5 style="color:black;">ID:</h5> ${id || 'N/A'}</div>
+     <div style="border-bottom: 1px solid black; padding: 5px;"><h5 style="color:black;">Classes:</h5> ${allClasses.join(', ') || 'N/A'}</div>
     `;
 }
 
-function moveBox(event){
-    console.log('Moving InfoBox running');
-    const x = event.pageX + 2; // Adjust to the right of the cursor
-    const y = event.pageY - 100; // Adjust below the cursor
+function moveBox(event) {
+    const x = event.pageX + 10;
+    const y = event.pageY - 200;
     Box.style.left = `${x}px`;
     Box.style.top = `${y}px`;
 }
 
-function createBox(){
+function createBox() {
     Box = document.createElement('div');
     Box.id = 'box';
     Box.style.position = 'absolute';
--- popup.js
+++ popup.js
@@ -4,10 +4,16 @@ inspectButton.addEventListener("click", async () => {
     try {
         let tabs = await chrome.tabs.query({ active: true, currentWindow: true });
         let tab = tabs[0];
-        console.log(tab);
         
-        // Send a message to the content script to perform an action
-        chrome.tabs.sendMessage(tab.id, { action: "inspectPage" });
+        // Sending a message to the content script to perform an action
+        chrome.tabs.sendMessage(tab.id, { action: "toggleInspection" }, (response) => {
+            if (response.isInspectionActive) {
+                inspectButton.innerText = "Close";
+            } else {
+                inspectButton.innerText = "Inspect";
+            }
+        });
+
     } catch (error) {
         console.error("Error fetching current tab", error);
     }
